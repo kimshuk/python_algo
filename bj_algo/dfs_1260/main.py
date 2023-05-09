2ee8c13bcdb66601427a6f4bb4e7efc37ccd2c46
Andrew Kim <jeesoo2002@gmail.com>
2023-05-09 18:42:10 +0900

    q26060 done: 바이러스

--- bj_algo/dfs_1260/main.py
+++ bj_algo/dfs_1260/main.py
@@ -1,19 +1,5 @@
-import math
-
-N, M = list(map(int, input().split(' ')))
-
-package = []
-single = []
-result = []
+N, M, V = list(map(int, input().split(' ')))
+lines = [[]]
 for _ in range(M):
-    p, s = list(map(int, input().split(' ')))
-    package.append(p)
-    single.append(s)
+    lines.append(list(map(int, input().split(' '))))
 
-package.sort()
-single.sort()
-
-result.append(package[0]*(math.ceil(N/6)))
-result.append(package[0]*(N//6)+single[0]*(N%6))
-result.append(single[0]*N)
-print(min(result))
