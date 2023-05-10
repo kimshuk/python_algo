7746de046028626c1087e32eb7f7d031ffa3fc57
Andrew Kim <jeesoo2002@gmail.com>
2023-05-11 00:42:18 +0900

    q1260 done: DFS와 BFS

--- bj_algo/dfs_1260/main.py
+++ bj_algo/dfs_1260/main.py
@@ -1,5 +1,48 @@
-N, M, V = list(map(int, input().split(' ')))
-lines = [[]]
+from collections import deque
+
+N, M, V = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited=[False]*(N+1)
+visited2=[False]*(N+1)
+
+
+print(f'before:{graph}')
+
+
 for _ in range(M):
-    lines.append(list(map(int, input().split(' '))))
+    a,b=map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in graph:
+    i.sort()
+
+print(f'completed:{graph}')
+# [[], [2, 3, 4], [1, 4], [1, 4], [1, 2, 3]]
+
+
+def dfs(curr):
+    print(curr, end=" ")
+    visited[curr] = True
+    for i in graph[curr]:
+        if not visited[i]:
+            dfs(i)
+
+
+def bfs(curr):
+    visited2[curr] = True
+    Q=deque([curr])
+    while Q:
+        c=Q.popleft()
+        print(c, end=" ")
+        for i in graph[c]:
+            if not visited2[i]:
+                Q.append(i)
+                visited2[i]=True
+
+
+dfs(V)
+print()
+bfs(V)
+
 
